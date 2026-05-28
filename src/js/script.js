1d4707d9048fdf5b6b822ecde8d038685c148a46
Lucas Couto <lucasdesouza16716@gmail.com>
2026-05-28 16:41:17 -0300

    teste final

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
 // ==========================================================================
-// AXIS - MAIN JS ENTRY POINT
+// AXIS — MAIN JS ENTRY POINT
 // ==========================================================================
 import "../sass/main.scss";
 
 // --------------------------------------------------------------------------
-// 1. Vendor (Libs / Imports)
+// Vendor (Libs / Imports)
 // --------------------------------------------------------------------------
 import Swiper from "swiper";
 import "swiper/css";
@@ -12,7 +12,7 @@ import "swiper/css/pagination";
 import { A11y, Keyboard, Pagination } from "swiper/modules";
 
 // --------------------------------------------------------------------------
-// 2. Swiper Carousels
+// Swiper Carousels
 // --------------------------------------------------------------------------
 const initServicesSwiper = () => {
   const swiperElement = document.querySelector(".services-swiper");
@@ -20,7 +20,7 @@ const initServicesSwiper = () => {
   // Trava de segurança: se não achar o carrossel na página, nem tenta rodar
   if (!swiperElement) return;
 
-  new Swiper(".services-swiper", {
+  new Swiper(swiperElement, {
     modules: [Pagination, A11y, Keyboard],
     slidesPerView: 1.2,
     spaceBetween: 10,
@@ -38,29 +38,19 @@ const initServicesSwiper = () => {
       enabled: true,
       prevSlideMessage: "Card de serviço anterior",
       nextSlideMessage: "Próximo card de serviço",
-      paginationBulletMessage: "Ir para o próximo card {{index}}",
+      paginationBulletMessage: "Ir para o card de serviço {{index}}",
     },
     breakpoints: {
       320: { slidesPerView: 1.3 },
-      350: { slidesPerView: 1.4 },
-      370: { slidesPerView: 1.5 },
-      390: { slidesPerView: 1.6 },
-      410: { slidesPerView: 2 },
-      430: { slidesPerView: 2.2 },
-      450: { slidesPerView: 2.3 },
-      470: { slidesPerView: 2.4 },
-      490: { slidesPerView: 2.5 },
-      510: { slidesPerView: 2.6 },
-      530: { slidesPerView: 2.7 },
-      550: { slidesPerView: 2.8 },
-      570: { slidesPerView: 2.9 },
+      400: { slidesPerView: 1.8 },
+      500: { slidesPerView: 2.4 },
       590: { slidesPerView: 3 },
     },
   });
 };
 
 // --------------------------------------------------------------------------
-// 3. Initialize
+// Initialize
 // --------------------------------------------------------------------------
 // Só dispara as funções quando o HTML terminar de carregar
 document.addEventListener("DOMContentLoaded", () => {
